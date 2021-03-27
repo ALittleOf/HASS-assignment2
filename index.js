2a92c48a26ce692d2e3fb55634f476239cfa8ab2
ALittleOf <ngqirongrachel@gmail.com>
2021-03-27 22:03:04 +0800

    little edits

--- index.js
+++
@@ -1,6 +0,0 @@
-let ageData = [
- { Type: "19 Y.O. to 21 Y.O.", Value: 30576 },
- { Type: "Above 21 Y.O.",Value: 117070 },
- { Type: "7 Y.O. to 19 Y.O.", Value: 21364 },
- { Type: "Unknown", Value: 159}
-]
